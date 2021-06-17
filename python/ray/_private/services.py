db133b480ede9c20af51413d1ba41b081f0344c8
Bernard Metzler <bmt@zurich.ibm.com>
2021-06-17 17:36:11 +0200

    Disable host default interface for cluster setup, if defined otherwise. Signed-off-by: Bernard Metzler <bmt@zurich.ibm.com>

--- python/ray/_private/services.py
+++ python/ray/_private/services.py
@@ -452,24 +452,29 @@ def node_ip_address_from_perspective(address):
         The IP address by which the local node can be reached from the address.
     """
     ip_address, port = address.split(":")
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    try:
-        # This command will raise an exception if there is no internet
-        # connection.
-        s.connect((ip_address, int(port)))
-        node_ip_address = s.getsockname()[0]
-    except OSError as e:
-        node_ip_address = "127.0.0.1"
-        # [Errno 101] Network is unreachable
-        if e.errno == errno.ENETUNREACH:
-            try:
-                # try get node ip address from host name
-                host_name = socket.getfqdn(socket.gethostname())
-                node_ip_address = socket.gethostbyname(host_name)
-            except Exception:
-                pass
-    finally:
-        s.close()
+    # bmt: do not rely on default host name as replied by google
+    if ip_address == "8.8.8.8":
+        my_host_name = socket.gethostname()
+        node_ip_address = socket.gethostbyname(my_host_name)
+    else:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        try:
+            # This command will raise an exception if there is no internet
+            # connection.
+            s.connect((ip_address, int(port)))
+            node_ip_address = s.getsockname()[0]
+        except OSError as e:
+            node_ip_address = "127.0.0.1"
+            # [Errno 101] Network is unreachable
+            if e.errno == errno.ENETUNREACH:
+                try:
+                    # try get node ip address from host name
+                    host_name = socket.getfqdn(socket.gethostname())
+                    node_ip_address = socket.gethostbyname(host_name)
+                except Exception:
+                    pass
+        finally:
+            s.close()
 
     return node_ip_address
 
